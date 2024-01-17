8c14453992ea88c75b699fe146e6d9101ffb27fd
Kashish Bhatia <kbhatiya999@gmail.com>
2024-01-17 11:50:17 +0530

    download code for media content added

--- whatsapp_bot_server/main.py
+++ whatsapp_bot_server/main.py
@@ -12,14 +12,16 @@ from whatsapp_bot_server.message_helper import WhatsAppMessenger
 wm = WhatsAppMessenger()
 
 print(pathlib.Path.cwd())
-static_dir = (pathlib.Path(__file__).parent / 'static')
-templates_dir = (pathlib.Path(__file__).parent / 'templates')
+static_dir = pathlib.Path(__file__).parent / "static"
+templates_dir = pathlib.Path(__file__).parent / "templates"
 print(static_dir)
 app = FastAPI()
 app.mount("/static", StaticFiles(directory=static_dir), name="static")
 
 
 templates = Jinja2Templates(directory=templates_dir)
+
+
 class WhatsAppMessage(BaseModel):
     # Define the structure of the message you expect from WhatsApp
     # This is a placeholder structure. Adjust it according to the actual API's specifications.
@@ -29,12 +31,12 @@ class WhatsAppMessage(BaseModel):
 
 @app.get("/webhook")
 async def verify_webhook(
-    hub_mode: Annotated[str, Query(alias='hub.mode')],
-    hub_challenge: Annotated[str, Query(alias='hub.challenge')],
-    hub_verify_token: Annotated[str, Query(alias='hub.verify_token')]
-        ):
+    hub_mode: Annotated[str, Query(alias="hub.mode")],
+    hub_challenge: Annotated[str, Query(alias="hub.challenge")],
+    hub_verify_token: Annotated[str, Query(alias="hub.verify_token")],
+):
     # Verify the token
-    if hub_mode == "subscribe" and hub_verify_token == 'happy':
+    if hub_mode == "subscribe" and hub_verify_token == "happy":
         # Respond with the challenge token from the request
         return Response(content=hub_challenge)
     else:
@@ -44,77 +46,93 @@ async def verify_webhook(
 
 @app.post("/webhook")
 async def receive_message(
-    data: dict
+    data: dict,
     # request: Request
-    ):
+):
     try:
-        top_level_data = data["entry"][0]['changes'][0]["value"]
+        top_level_data = data["entry"][0]["changes"][0]["value"]
 
-        if 'messages' in top_level_data:
-            message = top_level_data['messages'][0]
+        if "messages" in top_level_data:
+            message = top_level_data["messages"][0]
 
-            if message['type'] == 'text':
-                message_id = message['id']
-                message_text: str = message['text']['body']
-                message_from = message['from']
+            if message["type"] == "text":
+                message_id = message["id"]
+                message_text: str = message["text"]["body"]
+                message_from = message["from"]
                 # print(message)
                 print(message_id, message_from, message_text)
 
                 # verify
-                if message_text.startswith('/verify '):
-                    verification_status = message_text[8:] == 'ABCDEF'
+                if message_text.startswith("/verify "):
+                    verification_status = message_text[8:] == "ABCDEF"
                     if verification_status:
-                        message_text = 'Registered'
+                        message_text = "Registered"
                     else:
                         message_text = "Failed to Register"
 
-                data_send = wm.get_text_message_input(recipient=message_from,
-                                                       text=message_text,
-                                                        reply_to=message_id)
+                data_send = wm.get_text_message_input(
+                    recipient=message_from, text=message_text, reply_to=message_id
+                )
                 wm.send_message(data_send)
-            elif message['type'] == 'image':
-                message_id = message['id']
-                message_from = message['from']
-                image = message['image']
-                image_mime_type = image['mime_type']
-                image_id = image['id']
-                data_send = wm.get_image_message_input(recipient=message_from,
-                                                        image_link='https://lh3.googleusercontent.com/7MlzI0lw6bFS9zlH1PNs-8cGH9utkky2mqCNF3MX2QdMX6Cje0FEbsh5yHI70ZZ8CfwV2L8310OgneQ7ct6vZSlg=w640-h400-e365-rj-sc0x00ffffff',
-                                                        reply_to=message_id)
+            elif message["type"] == "image":
+                message_id = message["id"]
+                message_from = message["from"]
+                image = message["image"]
+                image_mime_type = image["mime_type"]
+                image_id = image["id"]
+                # downlaod
+                media_content = wm.get_and_download_media(image_id)
+                print(f"length of downloaded data {len(media_content)}")
+
+                data_send = wm.get_image_message_input(
+                    recipient=message_from,
+                    image_link="https://lh3.googleusercontent.com/7MlzI0lw6bFS9zlH1PNs-8cGH9utkky2mqCNF3MX2QdMX6Cje0FEbsh5yHI70ZZ8CfwV2L8310OgneQ7ct6vZSlg=w640-h400-e365-rj-sc0x00ffffff",
+                    reply_to=message_id,
+                )
                 wm.send_message(data_send)
-            elif message['type'] == 'document':
+            elif message["type"] == "document":
                 print(message)
 
             return {"response": f"Echo: {data}"}
         else:
-            print('It was some normal event - ignore')
+            print("It was some normal event - ignore")
 
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
-   
 
-@app.get('/', response_class=HTMLResponse)
+
+@app.get("/", response_class=HTMLResponse)
 def index(request: Request):
-    return templates.TemplateResponse(
-       name="index.html", context={'request':request}
-    )
+    return templates.TemplateResponse(name="index.html", context={"request": request})
 
-@app.post('/welcome')
+
+@app.post("/welcome")
 async def welcome():
-    data = wm.get_text_message_input(config.recipient_waid, 'Welcome to the Flight Confirmation Demo App for Python!')
+    data = wm.get_text_message_input(
+        config.recipient_waid, "Welcome to the Flight Confirmation Demo App for Python!"
+    )
 
     wm.send_message(data)
     return RedirectResponse(url="/catalog", status_code=303)
 
+
 @app.get("/catalog", response_class=HTMLResponse)
 async def catalog(request: Request):
     flights = get_flights()  # Call your function to get flights data
-    return templates.TemplateResponse('catalog.html', {"request": request, "title": "Flight Confirmation Demo for Python", "flights": flights})
+    return templates.TemplateResponse(
+        "catalog.html",
+        {
+            "request": request,
+            "title": "Flight Confirmation Demo for Python",
+            "flights": flights,
+        },
+    )
+
 
 @app.post("/buy-ticket")
-async def buy_ticket(flight_id: int = Form(..., alias='id')):
+async def buy_ticket(flight_id: int = Form(..., alias="id")):
     flights = get_flights()
-    flight = next(filter(lambda f: f['flight_id'] == flight_id, flights), None)
+    flight = next(filter(lambda f: f["flight_id"] == flight_id, flights), None)
 
     if not flight:
         # Handle the case where the flight is not found
@@ -128,8 +146,8 @@ async def buy_ticket(flight_id: int = Form(..., alias='id')):
     return RedirectResponse(url="/catalog", status_code=303)
 
 
-
 if __name__ == "__main__":
     import uvicorn
+
     # uvicorn.run('main:app', host="0.0.0.0", port=8000, reload=True)
     uvicorn.run(app, host="0.0.0.0", port=8000)
--- whatsapp_bot_server/message_helper.py
+++ whatsapp_bot_server/message_helper.py
@@ -4,16 +4,20 @@ import requests
 import json
 from config import config
 
+
 class WhatsAppMessenger:
     def __init__(self, recipient=None):
         self.config = SimpleNamespace()
-        self.config.version = os.getenv('', 'v18.0')
-        self.config.phone_number_id = os.getenv('', '223345670853370')
-        self.config.access_token = os.getenv('','EAACkxeo4HFcBO5vJP7GMzQ59alG0sJtfBNmci3UF4WGlNvk69uE6ygZClcoIfTcZA3IlWKR3bgt85q14oUuIFN0VFz9pN2Bmh9gl0qjIhnc0OQLu7YQDzs27yCIAzX6LJZCZALpAf7IAhvhFrhJgYndPJ3lUwnQINHS7TqZCP6JqQShgTE0UrNEXpZCV7NSZAhYBGnkYSGv6ekJJFqy9d50')
-        self.base_url = 'https://graph.facebook.com'
+        self.config.version = os.getenv("", "v18.0")
+        self.config.phone_number_id = os.getenv("", "223345670853370")
+        self.config.access_token = os.getenv(
+            "",
+            "EAACkxeo4HFcBO5vJP7GMzQ59alG0sJtfBNmci3UF4WGlNvk69uE6ygZClcoIfTcZA3IlWKR3bgt85q14oUuIFN0VFz9pN2Bmh9gl0qjIhnc0OQLu7YQDzs27yCIAzX6LJZCZALpAf7IAhvhFrhJgYndPJ3lUwnQINHS7TqZCP6JqQShgTE0UrNEXpZCV7NSZAhYBGnkYSGv6ekJJFqy9d50",
+        )
+        self.base_url = "https://graph.facebook.com"
         self.headers = {
             "Content-type": "application/json",
-            "Authorization": f"Bearer {self.config.access_token}"
+            "Authorization": f"Bearer {self.config.access_token}",
         }
         self.recipient = recipient
 
@@ -23,37 +27,31 @@ class WhatsAppMessenger:
             response = requests.post(url, data=data, headers=self.headers)
             if response.status_code == 200:
                 print("Status:", response.status_code)
-                print("Content-type:", response.headers['content-type'])
+                print("Content-type:", response.headers["content-type"])
                 print("Body:", response.text)
             else:
                 print(response.status_code)
                 print(response.text)
         except requests.ConnectionError as e:
-            print('Connection Error', str(e))
-    
-    
-    def get_text_message_input(self, recipient=None, text='', reply_to=None):
+            print("Connection Error", str(e))
+
+    def get_text_message_input(self, recipient=None, text="", reply_to=None):
         if not recipient:
             recipient = self.recipient
-        
+
         data = {
             "messaging_product": "whatsapp",
             "preview_url": False,
             "recipient_type": "individual",
             "to": recipient,
             "type": "text",
-            "text": {
-                "body": text
-            }
+            "text": {"body": text},
         }
         if reply_to:
-            data.update({
-                "context": {"message_id": reply_to}
-            })
+            data.update({"context": {"message_id": reply_to}})
         return json.dumps(data)
 
-
-    def get_image_message_input(self,recipient, image_link, reply_to=None):
+    def get_image_message_input(self, recipient, image_link, reply_to=None):
         if not recipient:
             recipient = self.recipient
         data = {
@@ -61,49 +59,84 @@ class WhatsAppMessenger:
             "recipient_type": "individual",
             "to": recipient,
             "type": "image",
-            "image": {
-                "link": image_link
-            }
+            "image": {"link": image_link},
         }
         if reply_to:
-            data.update({
-                "context": {"message_id": reply_to}
-            })
+            data.update({"context": {"message_id": reply_to}})
         return json.dumps(data)
 
-      
     def get_templated_message_input(self, recipient, flight):
-        return json.dumps({
-            "messaging_product": "whatsapp",
-            "to": recipient,
-            "type": "template",
-            "template": {
-                "name": "my_sample_flight_confirmation",
-                "language": {
-                    "code": "en_US"
-                },
-                "components": [
-                    {
-                        "type": "header",
-                        "parameters": [
-                            {
-                                "type": "document",
-                                "document": {
-                                    "filename": "FlightConfirmation.pdf",
-                                    "link": flight['document']
+        return json.dumps(
+            {
+                "messaging_product": "whatsapp",
+                "to": recipient,
+                "type": "template",
+                "template": {
+                    "name": "my_sample_flight_confirmation",
+                    "language": {"code": "en_US"},
+                    "components": [
+                        {
+                            "type": "header",
+                            "parameters": [
+                                {
+                                    "type": "document",
+                                    "document": {
+                                        "filename": "FlightConfirmation.pdf",
+                                        "link": flight["document"],
+                                    },
                                 }
-                            }
-                        ]
-                    },
-                    {
-                        "type": "body",
-                        "parameters": [
-                            {"type": "text", "text": flight['origin']},
-                            {"type": "text", "text": flight['destination']},
-                            {"type": "text", "text": flight['time']}
-                        ]
-                    }
-                ]
+                            ],
+                        },
+                        {
+                            "type": "body",
+                            "parameters": [
+                                {"type": "text", "text": flight["origin"]},
+                                {"type": "text", "text": flight["destination"]},
+                                {"type": "text", "text": flight["time"]},
+                            ],
+                        },
+                    ],
+                },
             }
-        })
+        )
 
+    def get_media_download_url(self, media_id):
+        url = f"{self.base_url}/{self.config.version}/{media_id}?phone_number_id={self.config.phone_number_id}"
+        try:
+            response = requests.get(url, headers=self.headers)
+            if response.status_code == 200:
+                download_url = response.json().get("url")
+                return download_url
+            else:
+                print("Error getting download URL:", response.status_code)
+                return None
+        except requests.ConnectionError as e:
+            print("Connection Error", str(e))
+            return None
+
+    def download_media_content(self, download_url):
+        try:
+            response = requests.get(download_url, headers=self.headers)
+            if response.status_code == 200:
+                return response.content
+            else:
+                print("Error downloading media:", response.status_code)
+                return None
+        except requests.ConnectionError as e:
+            print("Connection Error", str(e))
+            return None
+        
+    def get_and_download_media(self, media_id):
+        # Retrieve the download URL using the media ID
+        download_url = self.get_media_download_url(media_id)
+        if download_url:
+            # Download the media content using the download URL
+            media_content = self.download_media_content(download_url)
+            if media_content:
+                return media_content
+            else:
+                print("Failed to download media content")
+                return None
+        else:
+            print("Failed to get media download URL")
+            return None
